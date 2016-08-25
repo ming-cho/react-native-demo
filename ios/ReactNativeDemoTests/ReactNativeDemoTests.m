3c5f8fc40a7388f49a0d8ee534a6cc9dd36b2907
Mingxiang.Zhang <m.cho@opt.ne.jp>
2016-08-25 12:42:42 +0900

    rename project

--- ios/ReactNativeDemoTests/ReactNativeDemoTests.m
+++ ios/ReactNativeDemoTests/ReactNativeDemoTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface ReactNativeDemoTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation ReactNativeDemoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
